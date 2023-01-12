882854fae9c27f44f286e1ff52519e76253f0f88
annavlasand <anvlas1996@yandex.ru>
2023-01-12 19:28:06 +0300

    tasks

--- анализ данных о поездках на такси.py
+++ анализ данных о поездках на такси.py
@@ -1,8 +1,13 @@
 import pandas as pd
 taxi = pd.read_csv('2_taxi_nyc.csv', encoding='windows-1251', sep=',')
+#замена все пробелы в названии колонок на нижние подчеркивания:
 taxi.columns = taxi.columns.str.replace(' ', '_')
-#общее количество поездок без группировки
+#общее количество поездок без группировки:
 all_pickups = taxi['pickups'].sum()
-#название района с наименьшим числом поездок
+#название района с наименьшим числом поездок:
 min_pickups = taxi.groupby('borough').agg({'pickups': 'sum'}).idxmin()
-print(min_pickups)
+#группировка по району и праздничным дням:
+hd_pickups = taxi.groupby(['borough', 'hday']).agg({'pickups': 'mean'})
+#число поездок по месяцам для каждого района
+pickups_by_mon_bor = taxi.groupby(['borough', 'pickup_month'], as_index = False).agg({'pickups': 'sum'}).sort_values(by='pickups', ascending=False)
+print(pickups_by_mon_bor)
