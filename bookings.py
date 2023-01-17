17f66a711972634bfe34a612e35f098bfe82b667
annavlasand <anvlas1996@yandex.ru>
2023-01-17 13:59:14 +0300

    tasks

--- bookings.py
+++ bookings.py
@@ -4,3 +4,7 @@ bookings = pd.read_csv('bookings.csv', encoding='windows-1251', sep=';')
 bookings.columns = bookings.columns.str.replace(' ', '_')
 bookings.columns = bookings.columns.str.lower()
 #Пользователи из каких стран совершили наибольшее число успешных бронирований, укажите топ-5
+is_canceled = bookings.query('is_canceled == 0').country.value_counts()[:5]
+#На сколько ночей в среднем бронируют отели разных типов?
+nights = bookings.groupby('hotel').agg({'stays_total_nights': 'mean'}).round(2)
+
