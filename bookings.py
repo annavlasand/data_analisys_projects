5b850499084649fd3a45c471409c46841cba5d44
annavlasand <anvlas1996@yandex.ru>
2023-01-17 14:21:34 +0300

    tasks

--- bookings.py
+++ bookings.py
@@ -7,4 +7,18 @@ bookings.columns = bookings.columns.str.lower()
 is_canceled = bookings.query('is_canceled == 0').country.value_counts()[:5]
 #На сколько ночей в среднем бронируют отели разных типов?
 nights = bookings.groupby('hotel').agg({'stays_total_nights': 'mean'}).round(2)
+'''Иногда тип номера, полученного клиентом (assigned_room_type), отличается от изначально забронированного (reserved_room_type). 
+Такое может произойти, например, по причине овербукинга. 
+Сколько подобных наблюдений встретилось в датасете?
+*отмена бронирования также считается'''
+dif_room_types = bookings.query('assigned_room_type != reserved_room_type').shape
+'''Проанализируйте даты запланированного прибытия. 
+– На какой месяц чаще всего успешно оформляли бронь в 2016? 
+Изменился ли самый популярный месяц в 2017?
+– Сгруппируйте данные по годам и проверьте, на какой месяц бронирования отеля типа 
+City Hotel отменялись чаще всего в каждый из периодов.'''
+arrival_2016 = bookings.query('arrival_date_year == 2016').arrival_date_month.value_counts()
+arrival_2017 = bookings.query('arrival_date_year == 2017').arrival_date_month.value_counts()
+
+
 
