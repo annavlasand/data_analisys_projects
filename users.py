a985f634c790b4b108aff00d60f10faaf3296f73
annavlasand <anvlas1996@yandex.ru>
2023-01-18 21:29:52 +0300

    tasks

--- users.py
+++ users.py
@@ -18,4 +18,24 @@ successful_clients = logs_success.query('operations == @maximum_success') \
     .to_list() #формируем список из id клиентов, которые совершили максимальное количество успешных операций
 
 #С какой платформы было совершено наибольшее количество успешных операций?
+platform_success = logs.query('success == True') \
+    .groupby('platform', as_index = False) \
+    .agg({'success': 'count'}) \
+    .rename(columns={'success': 'operations'}) \
+    .sort_values(by=['operations'], ascending=False)
 
+#Какая платформа наиболее популярна среди премиумных клиентов?
+#объединяем таблицы logs и user_data
+platform_premium = logs.merge(user_data) \
+    .query('premium == True') \
+    .groupby('platform', as_index = False) \
+    .agg({'success': 'count'}) \
+    .rename(columns={'success': 'operations'}) \
+    .sort_values(by=['operations'], ascending=False)
+
+#распределение возраста клиентов в зависимости от типа клиента (премиум или нет)
+data = user_data.merge(logs)
+
+ax = sns.distplot(data.query('premium == True').age)
+ax = sns.distplot(data.query('premium == False').age)
+plt.show()
